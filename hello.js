aa8a7271d58308700d0523b7658fda601be1552d
Kim Taeyeong <freeptk1@gmail.com>
2022-02-15 21:18:53 +0900

    feat : tsc에 의해 컴파일 도중 컴파일 에러가 나는 코드

--- hello.js
+++ hello.js
@@ -1,1 +1,4 @@
-console.log("Hello world!");
+function greet(person, date) {
+    console.log("Hello ".concat(person, ", today is ").concat(date, "!"));
+}
+greet("Brendan");
--- hello.ts
+++ hello.ts
@@ -1,1 +1,5 @@
-console.log("Hello world!");
+function greet(person, date){
+    console.log(`Hello ${person}, today is ${date}!`);
+}
+
+greet("Brendan");
