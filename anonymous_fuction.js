0e30f14db6b80370d6212e6517bf95c27ec7f792
Kim Taeyeong <freeptk1@gmail.com>
2022-02-17 22:53:57 +0900

    feat : 익명함수의 매개변수 타입 추론
    매개변수의 타입이 지정되지 않았지만 TSC가 컴파일 과정에서 매개변수를 string으로 인식해서 toUppercase 함수가 잘된것을 인지함

---
+++ anonymous_fuction.js
@@ -0,0 +1,4 @@
+var names = ["Alice", "Bob", "Eve"];
+names.forEach(function (s) {
+    console.log(s.toUppercase());
+});
---
+++ anonymous_fuction.ts
@@ -0,0 +1,5 @@
+const names = ["Alice", "Bob", "Eve"];
+
+names.forEach(function (s) {
+    console.log(s.toUppercase());
+});
