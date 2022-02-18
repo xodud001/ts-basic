36cc4cb87f370c00a899c36d82bbb0926499b1bf
Kim Taeyeong <freeptk1@gmail.com>
2022-02-18 23:34:40 +0900

    feat : ?로 optional임을 표시

---
+++ optional_prop.js
@@ -0,0 +1,4 @@
+function printName(obj) {
+}
+printName({ first: "Bob" });
+printName({ first: "Bob", last: "Alisson" });
---
+++ optional_prop.ts
@@ -0,0 +1,6 @@
+function printName(obj: {first: string, last?: string}){
+
+}
+
+printName({first: "Bob"});
+printName({first: "Bob", last:"Alisson"});
